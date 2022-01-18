13cb9972b7c9b8c5f9123f8c8dfd887ef33e506f
Mesabe <bbgunbrits@gmail.com>
2022-01-18 09:28:20 +0200

    2

--- Q4/Patient.java
+++ Q4/Patient.java
@@ -6,12 +6,12 @@ import java.util.Date;
 public class Patient implements Serializable
 {
     private String pname;
-    private int page;
+    private String page;
     private String pcondition;
     private String pADate;
 
 
-    public Patient(String pname, int page, String pcondition, String pADate) {
+    public Patient(String pname, String page, String pcondition, String pADate) {
         this.pname = pname;
         this.page = page;
         this.pcondition = pcondition;
@@ -34,10 +34,10 @@ public class Patient implements Serializable
     public void setPcondition(String pcondition) {
         this.pcondition = pcondition;
     }
-    public int getPage() {
+    public String getPage() {
         return page;
     }
-    public void setPage(int page) {
+    public void setPage(String page) {
         this.page = page;
     }
     public void setPname(String pname) {
@@ -47,6 +47,6 @@ public class Patient implements Serializable
     @Override
     public String toString() {
         
-        return String.format(getPname() +"\t"+ Integer.toString(getPage()) + "\t\t" + getPcondition() + "\t\t" + getpADate()  );
+        return String.format(getPname() +"\t"+ getPage() + "\t\t" + getPcondition() + "\t\t" + getpADate());
     }
 }
--- Q4/Serverside.java
+++ Q4/Serverside.java
@@ -4,25 +4,61 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+enum Conditions{
+    HighlyCritical, Severe, Mild;
+}
 public class Serverside {
 
     public static void main(String[] args) throws IOException, Exception {
         ServerSocket ss = new ServerSocket(8080);
         System.out.println("Server is running==================");
         Socket cs = ss.accept();
-        DataInputStream inFromClient = new DataInputStream(cs.getInputStream());
-        ObjectOutputStream outToClient = new ObjectOutputStream(cs.getOutputStream());
+        DataInputStream inClient = new DataInputStream(cs.getInputStream());
+        ObjectOutputStream outClient = new ObjectOutputStream(cs.getOutputStream());
         String input = "";
 
         while (!input.equalsIgnoreCase("EXIT")) 
         {
-            input = inFromClient.readUTF();
+            input = inClient.readUTF();
             switch (input) {
                 case "DISPLAY_ALL":
-                    
+                    outClient.writeObject(getAllPatients());
                     break;
                 case "DISPLAY_Condition":
+                    displayConditions();
+                    input = inClient.readUTF();
+                    switch (input) {
+                        case "HighlyCritical":
+                            List<Patient> psHC = new ArrayList<>();
+                            for (Patient ps : getAllPatients()) {
+                                if (ps.getPcondition() == "Mild") {
+                                    psHC.add(ps);
+                                }
+                            }
+                            outClient.writeObject(psHC);
+                            break;
+                        case "Severe":
+                            List<Patient> psS = new ArrayList<>();
+                            for (Patient ps : getAllPatients()) {
+                                if (ps.getPcondition() == "Mild") {
+                                    psS.add(ps);
+                                }
+                            }
+                            outClient.writeObject(psS);
+                            break;
+                        case "Mild":
+                            List<Patient> psM = new ArrayList<>();
+                            for (Patient ps : getAllPatients()) {
+                                if (ps.getPcondition() == "Mild") {
+                                    psM.add(ps);
+                                }
+                            }
+                            outClient.writeObject(psM);
+                            break;
                     
+                        default:
+                            break;
+                    }
                     break;
                 case "EXIT":
                     
@@ -35,12 +71,34 @@ public class Serverside {
 
 
 
-    public static List<Patient> getAllPatients() 
+    public static List<Patient> getAllPatients() throws FileNotFoundException 
     {
         List<Patient> ps = new ArrayList<>();
+        File fs = new File("Patients.txt");
+        if (fs.exists()) {
+            Scanner sc = new Scanner(fs);
+            String line = "";
+            while (sc.hasNext()) {
+                line = sc.nextLine();
+                String[] values = line.split("@");
+                ps.add(new Patient(values[0], values[1], values[2], values[3]));
+                
+            }
+            sc.close();
+        } else {
+            System.err.println();
+        }
         
-
         return ps;
+    }
+
+
+    public static void displayConditions(){
+        System.out.println("\nMain Menu:");
+        for(int i = 0;i<Conditions.values().length;i++){
+            System.out.println(i+") "+Conditions.values()[i]);
+        }
+        System.out.print("Your choice: ");
     }
     
 }
